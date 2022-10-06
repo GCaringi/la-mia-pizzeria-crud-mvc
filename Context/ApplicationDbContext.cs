648aa806885ef248dd0d80ccd787ec587e48232a
GCaringi <giannicaringi94@gmail.com>
2022-10-06 14:28:19 +0200

    Initial commit

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using la_mia_pizzeria_crud_mvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_crud_mvc.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Pizza> Pizzas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=db-pizzeria;Integrated Security=True");
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_crud_mvc.Models;
+
+public class Pizza
+{
+    [Key]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Il campo è obbligatorio")]
+    public string Name { get; set; }
+    [Column(TypeName = "text")]
+    [Required(ErrorMessage = "Il campo è obbligatorio")]
+    [RegularExpression(@"(\w+\W){4,}\w+", ErrorMessage = "Inserisci almeno 5 parole")]
+    public string Description { get; set; }
+    [Required(ErrorMessage = "Il campo è obbligatorio")]
+    public string Image { get; set; }
+    [Required(ErrorMessage = "Il campo è obbligatorio")]
+    [Column(TypeName = "decimal(8,2)")]
+    [Range(1,100, ErrorMessage = "Il prezzo deve essere compreso tra 1 e 100")]
+    public decimal Price { get; set; }
+
+    public Pizza()
+    {
+        
+    }
+}
