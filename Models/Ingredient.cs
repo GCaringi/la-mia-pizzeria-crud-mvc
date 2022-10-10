5dc95ab38dbdb78b0cb1a1d38feef2c2c145f43c
GCaringi <giannicaringi94@gmail.com>
2022-10-10 14:45:12 +0200

    Add Models File and Migrations File

---
+++ Models/Ingredient.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_crud_mvc.Models;
+
+public class Ingredient
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<Pizza>? Pizzas { get; set; }
+
+    public Ingredient()
+    {
+        Pizzas = new List<Pizza>();
+    }
+}
